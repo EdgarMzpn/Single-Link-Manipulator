a9d29d8bce00985dcb41a67da30fc5698aa65e15
Ryuudai2610 <jesus.e.rdz@outlook.com>
2024-04-12 11:55:23 -0600

    Finished SLM simulator in ROS2

--- scripts/slm_sim.py
+++ scripts/slm_sim.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rclpy
 import numpy as np
+import time
 from rclpy.node import Node
 from rclpy.time import Time
 from std_msgs.msg import Float32, Header
@@ -30,12 +31,13 @@ class SLM_Sim(Node):
         # Define Publishers
         self.arm_pub = self.create_publisher(JointState, 'joint_states', 10)
         self.tau_pub = self.create_publisher(Float32, "tau", 10)
-
         self.get_logger().info("The SLM_Sim code is running")
+        time_period = 0.1 #seconds
+        self.timer = self.create_timer(time_period, self.sim)
         
     # Define the callback functions
     def callbackTau(self, msg):
-        self.Tau = msg
+        self.Tau = msg.data
 
     # Wrap to pi function
     def wrap_to_Pi(self, theta):
@@ -45,27 +47,40 @@ class SLM_Sim(Node):
         return result - np.pi
     
     # Run pendulum simulation
-    def run(self):
-        start_time = Time()
+    def sim(self):
+        # Start time
+        start_time = self.get_clock().now()
         while(1):
-            dt = Time() - start_time
+            # Get the current time
+            current_time = self.get_clock().now()
+            
+            # Calculate the duration between current_time and start_time
+            duration = current_time - start_time
+
+            # Convert the duration to a float value (in seconds)
+            self.dt = duration.nanoseconds * 1e-9
+            
             # SLM governing equation
-            self.x1 += self.x2*dt
+            self.x1 += self.x2 * self.dt
 
             x2_dot = (1/(self.J+self.m*self.a**2)) * (-self.m*self.g*self.a*np.cos(self.x1) - self.k*self.x2 + self.Tau)
-            self.x2 += x2_dot*dt
+            self.x2 += x2_dot*self.dt
             
             print("x1 = ", self.x1)
             print("tau = ", self.Tau)
 
-            self.position.header.stamp = Time()
+            self.position.header.stamp = self.get_clock().now().to_msg()
             self.position.name = ["joint2"]
             self.position.position = [self.x1]
             self.position.velocity = [self.x2]
             self.arm_pub.publish(self.position)
 
             if self.Tau > 0:
-                self.callbackTau(Float32(0))
+                self.tau_pub.publish(0)
+
+            start_time = self.get_clock().now()
+            time.sleep(0.1)
+
 
 # Initialize the Node
 def main(args=None):
